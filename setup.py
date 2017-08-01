4147a6303527761e1f3d762259466baf27239a3c
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-08-01 09:52:54 +0100

    Improve formatting of setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,17 @@ url = ""
 version = "0.1.0"
 readme = open('README.rst').read()
 
-setup(name="dtool-create",
-      packages=["dtool_create"],
-      version=version,
-      description="Dtool plugin for creating datasets and collections",
-      long_description=readme,
-      include_package_data=True,
-      author="Tjelvar Olsson",
-      author_email="tjelvar.olsson@jic.ac.uk",
-      url=url,
-      install_requires=[],
-      download_url="{}/tarball/{}".format(url, version),
-      license="MIT")
+setup(
+    name="dtool-create",
+    packages=["dtool_create"],
+    version=version,
+    description="Dtool plugin for creating datasets and collections",
+    long_description=readme,
+    include_package_data=True,
+    author="Tjelvar Olsson",
+    author_email="tjelvar.olsson@jic.ac.uk",
+    url=url,
+    install_requires=[],
+    download_url="{}/tarball/{}".format(url, version),
+    license="MIT"
+)
