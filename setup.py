63bc335af09330656277a43b8ec404e2ce796773
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-08-30 20:15:20 +0100

    Fix URL in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-url = "https://github.com/JIC-CSB/dtool-create"
+url = "https://github.com/jic-dtool/dtool-create"
 version = "0.2.0"
 readme = open('README.rst').read()
 
