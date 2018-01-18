797e5dae5f97599921b2f1cfeef3ca6088d8e935
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2018-01-18 17:25:43 +0000

    Fix dtoolcore version dependency

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     url=url,
     install_requires=[
         "click",
-        "dtoolcore>=2.8.1",
+        "dtoolcore>=3.0.0",
         "dtool_cli>=0.6.0",
         "ruamel.yaml",
     ],
