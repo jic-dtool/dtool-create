337d7d6e37835e7ba5a45dbcea814ad316cc4877
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2018-08-14 10:20:24 +0100

    Acutally deal with ruamel.yaml float values

--- dtool_create/dataset.py
+++ dtool_create/dataset.py
@@ -18,6 +18,7 @@ import dtoolcore.utils
 from ruamel.yaml import YAML
 from ruamel.yaml.comments import CommentedMap
 from ruamel.yaml.parser import ParserError
+from ruamel.yaml.scalarfloat import ScalarFloat
 
 from dtool_cli.cli import (
     base_dataset_uri_argument,
@@ -67,6 +68,31 @@ def _get_readme_template(fpath=None):
     )
 
     return readme_template
+
+
+def _prompt_for_values(d):
+    """Update the descriptive metadata interactively.
+
+    Uses values entered by the user. Note that the function keeps recursing
+    whenever a value is another ``CommentedMap`` or a ``list``. The
+    function works as passing dictionaries and lists into a function edits
+    the values in place.
+    """
+    for key, value in d.items():
+        if isinstance(value, CommentedMap):
+            _prompt_for_values(value)
+        elif isinstance(value, list):
+            for item in value:
+                _prompt_for_values(item)
+        else:
+            typ = type(value)
+
+            if isinstance(value, ScalarFloat):  # Deal with ruamel.yaml floats.
+                typ = float
+
+            new_value = click.prompt(key, type=typ, default=value)
+            d[key] = new_value
+    return d
 
 
 def _validate_name(name):
@@ -217,34 +243,12 @@ def interactive(proto_dataset_uri):
     yaml.indent(mapping=2, sequence=4, offset=2)
     descriptive_metadata = yaml.load(readme_template)
 
-    def prompt_for_values(d):
-        """Update the descriptive metadata interactively.
-
-        Uses values entered by the user. Note that the function keeps recursing
-        whenever a value is another ``CommentedMap`` or a ``list``. The
-        function works as passing dictionaries and lists into a function edits
-        the values in place.
-        """
-        for key, value in d.items():
-            if isinstance(value, CommentedMap):
-                prompt_for_values(value)
-            elif isinstance(value, list):
-                for item in value:
-                    prompt_for_values(item)
-            else:
-                new_value = click.prompt(key, type=type(value), default=value)
-                d[key] = new_value
-
-    prompt_for_values(descriptive_metadata)
+    descriptive_metadata = _prompt_for_values(descriptive_metadata)
 
     # Write out the descriptive metadata to the readme file.
     stream = StringIO()
 
-    # Deal with ruamel.yaml issue with floats!
-    try:
-        yaml.dump(descriptive_metadata, stream)
-    except TypeError:
-        yaml.dump(descriptive_metadata, stream)
+    yaml.dump(descriptive_metadata, stream)
 
     proto_dataset.put_readme(stream.getvalue())
 
