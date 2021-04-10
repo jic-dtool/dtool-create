9095f0ea9811b801bedbcc06337c71c2e5a30dc0
Tjelvar Olsson <tjelvar.olsson@gmail.com>
2021-04-10 12:46:37 +0100

    Update version number to 0.23.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,7 +55,7 @@ repo_name = u"dtool-create"
 # built documents.
 #
 # The short X.Y version.
-version = u"0.23.2"
+version = u"0.23.3"
 # The full version, including alpha/beta/rc tags.
 release = version
 
